f4410aa2837dbfbe3b50f3dd7e8063a112e5e067
sudhanshu khosla <sudhanshukhosla123@gmail.com>
2025-10-25 04:07:26 +0530

    sixth  commit

--- app/(MAIN)/admin/(main)/billing/_components/PayButton.jsx
+++ app/(MAIN)/admin/(main)/billing/_components/PayButton.jsx
@@ -1,68 +1,68 @@
-import { PayPalButtons } from '@paypal/react-paypal-js'
-import React from 'react'
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog.jsx"
-import { Button } from '@/components/ui/button'
-import { toast } from 'sonner'
-// import { useUser } from '@/app/provider'
-import { supabase } from '@/services/supabaseClient'
+// import { PayPalButtons } from '@paypal/react-paypal-js'
+// import React from 'react'
+// import {
+//     Dialog,
+//     DialogContent,
+//     DialogDescription,
+//     DialogHeader,
+//     DialogTitle,
+//     DialogTrigger,
+// } from "@/components/ui/dialog.jsx"
+// import { Button } from '@/components/ui/button'
+// import { toast } from 'sonner'
+// // import { useUser } from '@/app/provider'
+// import { supabase } from '@/services/supabaseClient'
 
-function PayButton({ amount, credits }) {
+// function PayButton({ amount, credits }) {
 
-    // const { user } = useUser();
-    const user = [];
+//     // const { user } = useUser();
+//     const user = [];
 
-    const onPaymentSuccess = async () => {
-        const { data, error } = await supabase
-            .from('Users')
-            .update({ credits: Number(user?.credits) + credits })
-            .eq('email', user?.email)
-            .select()
+//     const onPaymentSuccess = async () => {
+//         const { data, error } = await supabase
+//             .from('Users')
+//             .update({ credits: Number(user?.credits) + credits })
+//             .eq('email', user?.email)
+//             .select()
 
-        toast('Credit Updated');
-        window.location.reload();
-    }
+//         toast('Credit Updated');
+//         window.location.reload();
+//     }
 
-    return (
-        <div>
-            <Dialog>
-                <DialogTrigger asChild>
-                    <Button className="w-full">Purchase Credits</Button>
-                </DialogTrigger>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Checkout</DialogTitle>
-                        <DialogDescription asChild>
-                            <PayPalButtons style={{ layout: "vertical" }}
-                                onApprove={() => onPaymentSuccess()}
-                                onCancel={() => toast('Payment Canceled!')}
-                                createOrder={(data, actions) => {
-                                    return actions.order.create({
-                                        purchase_units: [
-                                            {
-                                                amount: {
-                                                    value: amount,
-                                                    currency_code: 'USD'
-                                                }
-                                            }
-                                        ]
-                                    })
-                                }}
-                            />
+//     return (
+//         <div>
+//             <Dialog>
+//                 <DialogTrigger asChild>
+//                     <Button className="w-full">Purchase Credits</Button>
+//                 </DialogTrigger>
+//                 <DialogContent>
+//                     <DialogHeader>
+//                         <DialogTitle>Checkout</DialogTitle>
+//                         <DialogDescription asChild>
+//                             <PayPalButtons style={{ layout: "vertical" }}
+//                                 onApprove={() => onPaymentSuccess()}
+//                                 onCancel={() => toast('Payment Canceled!')}
+//                                 createOrder={(data, actions) => {
+//                                     return actions.order.create({
+//                                         purchase_units: [
+//                                             {
+//                                                 amount: {
+//                                                     value: amount,
+//                                                     currency_code: 'USD'
+//                                                 }
+//                                             }
+//                                         ]
+//                                     })
+//                                 }}
+//                             />
 
-                        </DialogDescription>
-                    </DialogHeader>
-                </DialogContent>
-            </Dialog>
+//                         </DialogDescription>
+//                     </DialogHeader>
+//                 </DialogContent>
+//             </Dialog>
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default PayButton
+// export default PayButton
